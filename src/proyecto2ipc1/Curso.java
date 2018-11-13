52ffdc86c593866efb93d345f9526b32d011f964
USUARIO <USUARIO@LAPTOP-L8RSLLJN>
2018-11-13 13:33:15 -0600

    Comienzo1

---
+++ src/proyecto2ipc1/Curso.java
@@ -0,0 +1,12 @@
+
+package proyecto2ipc1;
+
+
+public class Curso {
+    int nCurso;
+    String catedratico;
+    String creditos;
+    String laboratorio;
+    String preRequisito;;
+    String PostRequisito;
+}
---
+++ src/proyecto2ipc1/Estudiante.java
@@ -0,0 +1,16 @@
+
+package proyecto2ipc1;
+
+
+public class Estudiante {
+    String carnet;
+    String dpi;
+    String Nombre;
+    String Apellido;
+    String correo;
+    String direccion;
+    String credito;
+    String contrasena;
+    
+    
+}
